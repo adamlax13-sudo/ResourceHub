da8100ce3843065be5c4bb11ddd2624fe404e262
adamlax13-sudo <adamlax13@gmail.com>
2026-01-15 01:25:12 +0000

    AY

--- server/index.ts
+++ server/index.ts
@@ -88,6 +88,7 @@ app.get("/", (req, res) => {
 
 // --- Start server ---
 const port = parseInt(process.env.PORT || "5000", 10);
-app.listen(port, () => console.log(`Server running on port ${port}`));
 
-export default app;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
